4a5ab295ff4c50ca5ad54fb87751b2e55c5dafd9
Stark94 <ciotta.pa@hotmail.it>
2018-10-26 14:11:28 +0200

    first

---
+++ corsospring/src/main/java/it/paolo/ciotta/CorsospringApplication.java
@@ -0,0 +1,12 @@
+package it.paolo.ciotta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorsospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorsospringApplication.class, args);
+	}
+}
